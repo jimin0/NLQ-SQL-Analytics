54e0331ff3664e3ed3d2fd61289f5e2aaffcd690
username <jiminkim0131@gmail.com>
2024-07-26 01:55:40 +0900

    Style: 주석 삭제

--- utils/config.py
+++ utils/config.py
@@ -18,19 +18,3 @@ def init_langsmith():
 
 def get_username():
     return st.session_state.get("username", "default_user")
-
-
-# from dotenv import load_dotenv
-# import os
-# from langchain_teddynote import logging
-
-# # 환경 변수 로드
-# load_dotenv()
-
-# # 설정 변수
-# MODEL_NAME = "gpt-4o-mini"
-# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-
-# def init_langsmith():
-#     logging.langsmith("wiset-project")
